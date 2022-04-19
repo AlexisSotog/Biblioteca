e815e96b8fe15ce81e0cf84e13c60bb01f239e61
kakatorox <felipetorresmora@outlook.es>
2022-04-18 20:41:24 -0400

    Creacion de Proyecto Booklet

---
+++ booklet/src/test/java/cl/desafiolatam/booklet/BookletApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.desafiolatam.booklet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
